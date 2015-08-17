5eff89cc37e421ce58180d68b71d03d98ed9a097
Pedro Henrique <system.pedrohenrique@gmail.com>
2015-08-16 21:50:30 -0300

    File and line numbers

--- LogInc.h
+++ LogInc.h
@@ -7,10 +7,8 @@ const std::string _LOG_FILE_NAME = "output.log";
 const bool _LOG_LOG_TIME         = false;
 
 #ifndef LOG
-#define LOG *ME::Log::getInstance(_LOG_WRITE_TO_FILE, _LOG_FILE_NAME, _LOG_LOG_TIME)
+#define LOG *ME::Log::getInstance(_LOG_WRITE_TO_FILE, _LOG_FILE_NAME, _LOG_LOG_TIME) << __FILE__ << ":" << __LINE__ << " "
 #endif
-
-
 
 #endif // LOGINC_H
 
